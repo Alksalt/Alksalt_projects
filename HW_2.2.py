b45034674e738b0d60093f4b5f7718e1ea365cf3
alksalt <ogano0296@gmail.com>
2024-05-23 13:07:35 +0200

    commit

--- HW_2.2.py
+++ HW_2.2.py
@@ -5,7 +5,9 @@ a,piece3 = divmod(piece1,100)
 b, c =divmod(piece3,10)
 d,f = divmod(piece2,10)
 a,b,c,d,f = f,d,c,b,a
-print(str(a)+str(b)+str(c)+str(d)+str(f))
+reversed_number = a * 10000 + b * 1000 + c * 100 + d * 10 + f
+
+print(reversed_number)
 
 
 
